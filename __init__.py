180166310faa2d20aeca0be76597607025716d93
Lord Mike <lordmike@iki.fi>
2020-03-09 19:45:32 +0200

    module init file

---
+++ __init__.py
@@ -0,0 +1,24 @@
+# -*- coding: UTF-8 -*-
+"""
+    app_source_handler
+    ~~~~~~~~~~~~~~~~~~
+
+    Read/write 'app_source' xml file content.
+
+    License of this script file:
+       MIT License
+
+    License is available online:
+      https://github.com/lordmikefin/app_source_handler/blob/master/LICENSE
+
+    Latest version of this script file:
+      https://github.com/lordmikefin/app_source_handler/blob/master/__init__.py
+
+
+    :copyright: (c) 2020, Mikko Niemelä a.k.a. Lord Mike (lordmike@iki.fi)
+    :license: MIT License
+"""
+
+__license__ = "MIT License"
+__version__ = "0.0.1"
+__revision__ = "app_source_handler (module)  v" + __version__ + " (2020-03-09)"
