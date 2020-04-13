fa1da65b1d048d9c17eae30defc213708e62d64d
Lord Mike <lordmike@iki.fi>
2020-04-13 18:12:14 +0300

    NOTE: This code is tested only with Python version 3.7

--- __init__.py
+++ __init__.py
@@ -23,4 +23,9 @@ __license__ = "MIT License"
 __version__ = "0.0.7"
 __revision__ = "app_source_handler (module)  v" + __version__ + " (2020-04-06)"
 
+import sys
+
+# NOTE: This code is tested only with Python version 3.7
+assert sys.version_info >= (3, 7)
+
 from . import source
