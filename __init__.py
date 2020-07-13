e9acb0054a8748ec5e78d1bf897e89cfc2d41761
Lord Mike <lordmike@iki.fi>
2020-07-13 15:57:42 +0300

    version 0.0.15

--- __init__.py
+++ __init__.py
@@ -20,8 +20,8 @@
 """
 
 __license__ = "MIT License"
-__version__ = "0.0.14"
-__revision__ = "app_source_handler (module)  v" + __version__ + " (2020-07-10)"
+__version__ = "0.0.15"
+__revision__ = "app_source_handler (module)  v" + __version__ + " (2020-07-13)"
 
 import sys
 
